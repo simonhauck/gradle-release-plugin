37d8ffc5eeb70b342499e1629cb0af59c87baf50
SimonHauck <simonhauck@gmx.de>
2024-01-27 13:45:36 +0100

    Add frontend api generation

---
+++ server-api/build.gradle.kts
@@ -0,0 +1,34 @@
+import org.openapitools.generator.gradle.plugin.tasks.GenerateTask
+
+plugins { alias(libs.plugins.openApiGenerator) }
+
+openApiGenerate {
+    generatorName.set("typescript-fetch")
+    inputSpec.set(layout.projectDirectory.file("src/main/resources/openapi.json").asFile.path)
+    outputDir.set(layout.buildDirectory.dir("/generated/typescript-fetch").get().asFile.path)
+
+    configOptions.set(
+        mapOf("npmName" to "@server/api", "npmVersion" to "1.0.0", "supportsES6" to "true")
+    )
+}
+
+tasks.register("clean") { delete(layout.buildDirectory) }
+
+val deleteGeneratedCodeTask =
+    tasks.register<Delete>("cleanApiGeneratedPackages") {
+        delete(layout.buildDirectory.dir("/generated"))
+    }
+
+val generateApiBindingTask = tasks.withType<GenerateTask> { dependsOn(deleteGeneratedCodeTask) }
+
+val zipTypeScriptFetchApiTask =
+    tasks.register<Zip>("zipTypeScriptFetchApi") {
+        dependsOn(generateApiBindingTask)
+        from(layout.buildDirectory.dir("/generated/typescript-fetch"))
+        destinationDirectory.set(layout.buildDirectory.dir("zip"))
+        archiveBaseName.set("typescript-fetch-client")
+    }
+
+tasks.register("assemble") { dependsOn(zipTypeScriptFetchApiTask) }
+
+configurations.create("zip").let { artifacts.add(it.name, zipTypeScriptFetchApiTask.get()) }
