8749511a579e60efd29231fff4c228b7687e843f
SimonHauck <simonhauck@gmx.de>
2024-02-03 00:41:23 +0100

    add example logging

--- server/src/main/kotlin/com/github/cryptojuenger/template/server/ServerApplication.kt
+++ server/src/main/kotlin/com/github/cryptojuenger/template/server/ServerApplication.kt
@@ -1,16 +1,20 @@
 package com.github.cryptojuenger.template.server
 
+import io.github.oshai.kotlinlogging.KotlinLogging
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
+private val log = KotlinLogging.logger {}
+
 @RestController
 @RequestMapping("/api/example")
 class ExampleController {
     @GetMapping()
     fun helloWorld(): HelloWorld {
+        log.info { "this is a log entry" }
         return HelloWorld("CryptoJuenger are the best")
     }
 }
