640ae47b54930983e846b8cbd6ceae3f70b07bdf
Simon Hauck <simonhauck@gmx.de>
2024-02-09 23:46:25 +0100

    streamline api generation by removing unnecessary delete task, mark clean task as Type Delete

--- server-api/build.gradle.kts
+++ server-api/build.gradle.kts
@@ -1,30 +1,23 @@
-import org.openapitools.generator.gradle.plugin.tasks.GenerateTask
-
 plugins { alias(libs.plugins.openApiGenerator) }
+
+val generateApiBaseDir = "generated/typescript-fetch"
 
 openApiGenerate {
     generatorName.set("typescript-fetch")
     inputSpec.set(layout.projectDirectory.file("src/main/resources/openapi.json").asFile.path)
-    outputDir.set(layout.buildDirectory.dir("generated/typescript-fetch").map { it.asFile.path })
-
+    outputDir.set(layout.buildDirectory.dir(generateApiBaseDir).get().asFile.path)
+    cleanupOutput = true
     configOptions.set(
         mapOf("npmName" to "@server/api", "npmVersion" to "1.0.0", "supportsES6" to "true")
     )
 }
 
-tasks.register("clean") { delete(layout.buildDirectory) }
-
-val deleteGeneratedCodeTask =
-    tasks.register<Delete>("cleanApiGeneratedPackages") {
-        delete(layout.buildDirectory.dir("generated/typescript-fetch/*"))
-    }
-
-val generateApiBindingTask = tasks.withType<GenerateTask> { dependsOn(deleteGeneratedCodeTask) }
+tasks.register<Delete>("clean") { delete(layout.buildDirectory) }
 
 val zipTypeScriptFetchApiTask =
     tasks.register<Zip>("zipTypeScriptFetchApi") {
-        dependsOn(generateApiBindingTask)
-        from(layout.buildDirectory.dir("generated/typescript-fetch/src"))
+        dependsOn(tasks.openApiGenerate)
+        from(layout.buildDirectory.dir("$generateApiBaseDir/src"))
         destinationDirectory.set(layout.buildDirectory.dir("zip"))
         archiveBaseName.set("typescript-fetch-client")
     }
