05491152c83c55528ed44db52e71a83d4a533623
Simon Hauck <simonhauck@gmx.de>
2024-02-09 22:59:37 +0100

    fix formatting

--- web-nextjs/src/app/test/page.tsx
+++ web-nextjs/src/app/test/page.tsx
@@ -4,7 +4,7 @@ export default async function Page() {
   return (
     <>
       <div className={"border border-red-500 p-5"}></div>
-      <div>Hallo Simmen! This is a server component!</div>
+      <div>Hallo Simmen! This is a server component! </div>
       <div>Ich bins Tobi</div>
       <Button></Button>
     </>
