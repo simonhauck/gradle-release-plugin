497ab4521971eb2e0e5d06f8c657e2e6a5413915
Simon Hauck <simonhauck@gmx.de>
2024-02-10 17:49:25 +0100

    close inputstream after reading file

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,21 +1,13 @@
-import java.io.File
-import java.io.FileInputStream
 import java.util.*
 
-plugins {
-    id("build.common.kotlin-conventions") apply false
-}
+plugins { id("build.common.kotlin-conventions") apply false }
 
 allprojects {
     group = "com.github.cryptojuenger.template"
     version = readVersionFromFile(file("${rootProject.projectDir.resolve("version.properties")}"))
 }
 
-fun readVersionFromFile(file: File): String {
-    val fis = FileInputStream(file)
-    val prop = Properties()
-    prop.load(fis)
+private fun readVersionFromFile(file: File): String = readProperties(file).getProperty("version")
 
-    return prop.getProperty("version")
-}
-
+private fun readProperties(propertiesFile: File) =
+    Properties().apply { propertiesFile.inputStream().use { load(it) } }
