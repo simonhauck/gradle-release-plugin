ee06da1b8e483c3da4da039ccbe2c22f1a144ad9
Simon Hauck <simonhauck@gmx.de>
2024-05-18 01:09:44 +0200

    #21 disable release task for example when tasks from parent are started

--- example/build.gradle.kts
+++ example/build.gradle.kts
@@ -1,3 +1,4 @@
+import io.github.simonhauck.release.tasks.BaseReleaseTask
 import io.github.simonhauck.release.version.api.Version
 
 plugins {
@@ -6,6 +7,12 @@ plugins {
 
     // You would have to additionally specify the version
     id("io.github.simonhauck.release")
+}
+
+// This is just here, because it interferes with the release task of this parent project
+tasks.withType<BaseReleaseTask>() {
+    val inlcude = project.path.startsWith("example")
+    onlyIf { inlcude }
 }
 
 version = Version.fromPropertiesFile(layout.projectDirectory.file("version.properties").asFile)
