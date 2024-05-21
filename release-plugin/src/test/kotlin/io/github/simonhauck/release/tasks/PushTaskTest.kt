ab6c228a2b6943650a7bd466ef67c773b7da35dc
Simon Hauck <simon.hauck@bruker.com>
2024-05-21 22:03:23 +0200

    #32 add tests for gitPush task, throw exceptions in case a git operation fails

---
+++ release-plugin/src/test/kotlin/io/github/simonhauck/release/tasks/PushTaskTest.kt
@@ -0,0 +1,77 @@
+package io.github.simonhauck.release.tasks
+
+import io.github.simonhauck.release.testdriver.ReleasePluginTestDriver
+import io.github.simonhauck.release.testdriver.assertIsOk
+import java.io.File
+import org.assertj.core.api.Assertions.assertThat
+import org.gradle.testkit.runner.TaskOutcome
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.io.TempDir
+
+class PushTaskTest {
+
+    @TempDir lateinit var tmpDir: File
+    private val testDriver = ReleasePluginTestDriver()
+
+    @Test
+    fun `should push the commit to the remote repository`() =
+        testDriver(tmpDir) {
+            createValidRepositoryWithRemote()
+
+            appendContentToBuildGradle(
+                """
+                |tasks.register<PushTask>("testPush"){}
+                """
+                    .trimMargin()
+            )
+            client1Api.add("build.gradle.kts")
+            client1Api.commit("pushTaskMessage")
+
+            val runner = testKitRunner().withArguments("testPush").build()
+
+            val actual = runner.task(":testPush")?.outcome
+            assertThat(actual).isEqualTo(TaskOutcome.SUCCESS)
+
+            cloneForClient2()
+            val latestCommit = client2Api.log().assertIsOk().last()
+            assertThat(latestCommit.message).isEqualTo("pushTaskMessage")
+        }
+
+    @Test
+    fun `should fail if there is no remote origin available`() =
+        testDriver(tmpDir) {
+            appendContentToBuildGradle(
+                """
+                |tasks.register<PushTask>("testPush"){}
+                """
+                    .trimMargin()
+            )
+
+            createLocalRepository()
+
+            val runner = testKitRunner().withArguments("testPush").buildAndFail()
+
+            val actual = runner.task(":testPush")?.outcome
+            assertThat(actual).isEqualTo(TaskOutcome.FAILED)
+        }
+
+    @Test
+    fun `should not fail if no valid repository is available but disable push is true`() =
+        testDriver(tmpDir) {
+            appendContentToBuildGradle(
+                """
+            |tasks.register<PushTask>("testPush"){
+            |   disablePush = true
+            |}
+            """
+                    .trimMargin()
+            )
+
+            createLocalRepository()
+
+            val runner = testKitRunner().withArguments("testPush").build()
+
+            val actual = runner.task(":testPush")?.outcome
+            assertThat(actual).isEqualTo(TaskOutcome.SUCCESS)
+        }
+}
