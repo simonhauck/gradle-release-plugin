d779da3d6faa85d8f3ddfcdcbda1ca08fa5c356d
Simon Hauck <simon.hauck@bruker.com>
2024-05-21 16:48:41 +0200

    #32 restructure tests to use testDriver

--- release-plugin/src/test/kotlin/io/github/simonhauck/release/git/api/GitCommandApiTest.kt
+++ release-plugin/src/test/kotlin/io/github/simonhauck/release/git/api/GitCommandApiTest.kt
@@ -1,247 +1,218 @@
 package io.github.simonhauck.release.git.api
 
-import io.github.simonhauck.release.git.internal.commands.GitCommandProcessWrapper
-import io.github.simonhauck.release.git.internal.process.ProcessConfig
-import io.github.simonhauck.release.git.internal.process.ProcessWrapper
-import io.github.simonhauck.release.testdriver.GitTestCommandService
+import io.github.simonhauck.release.testdriver.ReleasePluginTestDriver
 import io.github.simonhauck.release.testdriver.assertIsOk
 import io.github.simonhauck.release.testdriver.get
 import java.io.File
 import org.assertj.core.api.Assertions.assertThat
-import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.io.TempDir
 
 class GitCommandApiTest {
 
-    @TempDir lateinit var tempDir: File
-    private lateinit var gitCommandApi: GitCommandApi
-    private lateinit var processWrapper: ProcessWrapper
+    @TempDir lateinit var tmpDir: File
 
-    @BeforeEach
-    fun setup() {
-        gitCommandApi = GitCommandProcessWrapper(config = ProcessConfig(workingDir = tempDir))
-        processWrapper = ProcessWrapper()
-    }
+    private val testDriver = ReleasePluginTestDriver()
 
     @Test
-    fun `can create a git repository and correctly return the status`() {
-        gitCommandApi.init("main").assertIsOk()
+    fun `can create a git repository and correctly return the status`() =
+        testDriver(tmpDir) {
+            client1Api.init("main").assertIsOk()
 
-        gitCommandApi.status().assertIsOk()
-    }
-
-    @Test
-    fun `check that the git command fails if no git repository is available`() {
-        val actual = gitCommandApi.status()
-
-        assertThat(actual.isOk()).isFalse()
-    }
-
-    @Test
-    fun `should be able to create a git repository and commit a file`() {
-        gitCommandApi.init("main")
-
-        val file = File(tempDir, "file.txt")
-        file.writeText("Hello World")
-
-        gitCommandApi.add("file.txt").assertIsOk()
-        gitCommandApi.commit("Initial commit").assertIsOk()
-
-        val actual = gitCommandApi.log().get().map { it.message }
-        assertThat(actual).containsExactly("Initial commit")
-    }
-
-    @Test
-    fun `git log should return the newest commit as last element`() {
-        gitCommandApi.init("main")
-
-        val file = File(tempDir, "file.txt")
-        file.writeText("Hello World")
-
-        gitCommandApi.add("file.txt").assertIsOk()
-        gitCommandApi.commit("Initial commit").assertIsOk()
-
-        file.writeText("new text")
-        gitCommandApi.add("file.txt").assertIsOk()
-        gitCommandApi.commit("Second commit").assertIsOk()
-
-        val actual = gitCommandApi.log().get().map { it.message }
-        assertThat(actual).containsExactly("Initial commit", "Second commit")
-    }
-
-    @Test
-    fun `should correctly resolve the status of multiple files`() {
-        setupGitRepoWithInitialCommit()
-
-        File("$tempDir/unstaged.txt").writeText("unstaged")
-        gitCommandApi.add(".").assertIsOk()
-        gitCommandApi.commit("Commit file").assertIsOk()
-
-        File("$tempDir/untracked.txt").writeText("Untracked")
-        File("$tempDir/unstaged.txt").writeText("unstaged2")
-        File("$tempDir/staged.txt").writeText("Staged")
-        gitCommandApi.add("staged.txt").assertIsOk()
-
-        val actual = gitCommandApi.status().assertIsOk()
-        assertThat(actual.staged).containsExactly("staged.txt")
-        assertThat(actual.untracked).containsExactly("untracked.txt")
-        assertThat(actual.unstaged).containsExactly("unstaged.txt")
-    }
-
-    @Test
-    fun `should reset the local changes so that the file is untracked again`() {
-        setupGitRepoWithInitialCommit()
-
-        File("$tempDir/someFile.txt").writeText("Hello World")
-        gitCommandApi.add("someFile.txt").assertIsOk()
-        val beforeCheck = gitCommandApi.status().assertIsOk()
-        assertThat(beforeCheck.staged).containsExactly("someFile.txt")
-
-        gitCommandApi.reset("someFile.txt").assertIsOk()
-
-        val actual = gitCommandApi.status().assertIsOk()
-        assertThat(actual.staged).isEmpty()
-        assertThat(actual.untracked).containsExactly("someFile.txt")
-    }
-
-    @Test
-    fun `should contain the names of all local created branches`() {
-        setupGitRepoWithInitialCommit()
-
-        gitCommandApi.createBranch("feature-1").assertIsOk()
-        gitCommandApi.createBranch("feature-2").assertIsOk()
-
-        val actual = gitCommandApi.getLocalBranchNames().assertIsOk()
-        assertThat(actual).contains("feature-1", "feature-2")
-    }
-
-    @Test
-    fun `should be able to delete branches`() {
-        setupGitRepoWithInitialCommit()
-
-        gitCommandApi.createBranch("feature-1").assertIsOk()
-        gitCommandApi.createBranch("feature-2").assertIsOk()
-
-        gitCommandApi.deleteBranch("feature-1").assertIsOk()
-
-        val actualBranches = gitCommandApi.getLocalBranchNames().assertIsOk()
-        assertThat(actualBranches).doesNotContain("feature-1")
-    }
-
-    @Test
-    fun `the file should contain the original content after the commit was deleted`() {
-        setupGitRepoWithInitialCommit()
-
-        repeat(2) {
-            File("$tempDir/newFile.txt").writeText("Commit number $it")
-            gitCommandApi.add("newFile.txt")
-            gitCommandApi.commit("commit number $it")
+            client1Api.status().assertIsOk()
         }
 
-        val commandResult = gitCommandApi.deleteLastCommit()
-        assertThat(commandResult.isOk()).isTrue()
+    @Test
+    fun `check that the git command fails if no git repository is available`() =
+        testDriver(tmpDir) {
+            val actual = client1Api.status()
 
-        val actual = File("$tempDir/newFile.txt").readText()
-        assertThat(actual).isEqualTo("Commit number 0")
-    }
+            assertThat(actual.isOk()).isFalse()
+        }
 
     @Test
-    fun `the file should be deleted when the commit is reverted where the file was added`() {
-        setupGitRepoWithInitialCommit()
+    fun `should be able to create a git repository and commit a file`() =
+        testDriver(tmpDir) {
+            client1Api.init("main")
 
-        File("$tempDir/newFile.txt").writeText("Hello World")
-        gitCommandApi.add("newFile.txt").assertIsOk()
-        gitCommandApi.commit("Add new file").assertIsOk()
+            client1Api.add(".").assertIsOk()
+            client1Api.commit("Initial commit").assertIsOk()
 
-        gitCommandApi.deleteLastCommit().assertIsOk()
-
-        val actual = File("$tempDir/newFile.txt").exists()
-        assertThat(actual).isFalse()
-    }
+            val actual = client1Api.log().get().map { it.message }
+            assertThat(actual).containsExactly("Initial commit")
+        }
 
     @Test
-    fun `should be able to set a remote branch and push a git commit that is remote available`(
-        @TempDir remoteDir: File
-    ) {
-        val gitRepoDirectory = remoteDir.resolve("remote").apply { mkdir() }
-        val gitClientDirectory = remoteDir.resolve("client").apply { mkdir() }
-        val gitSererCommands = GitTestCommandService(gitRepoDirectory)
-        val otherClientCommands = GitTestCommandService(gitClientDirectory)
+    fun `git log should return the newest commit as last element`() =
+        testDriver(tmpDir) {
+            createLocalRepository()
 
-        gitSererCommands.initBareRepository().assertIsOk()
+            appendContentToBuildGradle("some content")
+            client1Api.add(".").assertIsOk()
+            client1Api.commit("Second commit").assertIsOk()
 
-        setupGitRepoWithInitialCommit()
-        gitCommandApi.addRemoteAndSetUpstream("origin", gitRepoDirectory.absolutePath, "main")
-        gitCommandApi.push().assertIsOk()
-
-        // Check can the branch be checked out
-        otherClientCommands.clone(gitRepoDirectory.absolutePath, ".", "main").assertIsOk()
-        assertThat(gitClientDirectory.resolve("file.txt").readText()).isEqualTo("Hello World")
-    }
+            val actual = client1Api.log().get().map { it.message }
+            assertThat(actual).containsExactly("Initial commit", "Second commit")
+        }
 
     @Test
-    fun `git push should fail if another client has already pushed a commit`(
-        @TempDir remoteDir: File
-    ) {
-        val gitRepoDirectory = remoteDir.resolve("remote").apply { mkdir() }
-        val gitClientDirectory = remoteDir.resolve("client").apply { mkdir() }
+    fun `should correctly resolve the status of multiple files`() =
+        testDriver(tmpDir) {
+            createLocalRepository()
 
-        val gitServerCommands = GitTestCommandService(gitRepoDirectory)
-        val otherClientCommands = GitTestCommandService(gitClientDirectory)
+            File("$client1WorkDir/unstaged.txt").writeText("unstaged")
+            client1Api.add(".").assertIsOk()
+            client1Api.commit("Commit file").assertIsOk()
 
-        gitServerCommands.initBareRepository().assertIsOk()
+            File("$client1WorkDir/untracked.txt").writeText("Untracked")
+            File("$client1WorkDir/unstaged.txt").writeText("unstaged2")
+            File("$client1WorkDir/staged.txt").writeText("Staged")
+            client1Api.add("staged.txt").assertIsOk()
 
-        setupGitRepoWithInitialCommit()
-        gitCommandApi.addRemoteAndSetUpstream("origin", gitRepoDirectory.absolutePath, "main")
-        gitCommandApi.push().assertIsOk()
-
-        // Push from another client
-        otherClientCommands.clone(gitRepoDirectory.absolutePath, ".", "main").assertIsOk()
-        gitClientDirectory.resolve("other.txt").apply { writeText("Hello World") }
-        otherClientCommands.add("other.txt").assertIsOk()
-        otherClientCommands.commit("Second commit").assertIsOk()
-        otherClientCommands.push().assertIsOk()
-
-        // Original client should fail
-        tempDir.resolve("something.txt").apply { writeText("Hello World") }
-        gitCommandApi.add("something.txt").assertIsOk()
-        gitCommandApi.commit("Third commit").assertIsOk()
-        val actual = gitCommandApi.push()
-
-        assertThat(actual.isLeft()).isTrue()
-    }
+            val actual = client1Api.status().assertIsOk()
+            assertThat(actual.staged).containsExactly("staged.txt")
+            assertThat(actual.untracked).containsExactly("untracked.txt")
+            assertThat(actual.unstaged).containsExactly("unstaged.txt")
+        }
 
     @Test
-    fun `should be able to create and list tags`() {
-        setupGitRepoWithInitialCommit()
+    fun `should reset the local changes so that the file is untracked again`() =
+        testDriver(tmpDir) {
+            createLocalRepository()
 
-        gitCommandApi.tag("v1.0", "Initial release").assertIsOk()
-        gitCommandApi.tag("v1.1", "Second release").assertIsOk()
+            File("$client1WorkDir/someFile.txt").writeText("Hello World")
+            client1Api.add("someFile.txt").assertIsOk()
+            val beforeCheck = client1Api.status().assertIsOk()
+            assertThat(beforeCheck.staged).containsExactly("someFile.txt")
 
-        val actual = gitCommandApi.listTags().assertIsOk()
+            client1Api.reset("someFile.txt").assertIsOk()
 
-        assertThat(actual).containsExactly("v1.0", "v1.1")
-    }
+            val actual = client1Api.status().assertIsOk()
+            assertThat(actual.staged).isEmpty()
+            assertThat(actual.untracked).containsExactly("someFile.txt")
+        }
 
     @Test
-    fun `should be able to delete a tag`() {
-        setupGitRepoWithInitialCommit()
+    fun `should contain the names of all local created branches`() =
+        testDriver(tmpDir) {
+            createLocalRepository()
 
-        gitCommandApi.tag("v1.0", "Initial release").assertIsOk()
-        gitCommandApi.tag("v1.1", "Second release").assertIsOk()
+            client1Api.createBranch("feature-1").assertIsOk()
+            client1Api.createBranch("feature-2").assertIsOk()
 
-        gitCommandApi.deleteLocalTag("v1.0").assertIsOk()
+            val actual = client1Api.getLocalBranchNames().assertIsOk()
+            assertThat(actual).contains("feature-1", "feature-2")
+        }
 
-        val actual = gitCommandApi.listTags().assertIsOk()
-        assertThat(actual).containsExactly("v1.1")
-    }
+    @Test
+    fun `should be able to delete branches`() =
+        testDriver(tmpDir) {
+            createLocalRepository()
 
-    private fun setupGitRepoWithInitialCommit() {
-        gitCommandApi.init("main")
+            client1Api.createBranch("feature-1").assertIsOk()
+            client1Api.createBranch("feature-2").assertIsOk()
 
-        File("$tempDir/file.txt").writeText("Hello World")
-        gitCommandApi.add("file.txt")
-        gitCommandApi.commit("Initial commit")
-    }
+            client1Api.deleteBranch("feature-1").assertIsOk()
+
+            val actualBranches = client1Api.getLocalBranchNames().assertIsOk()
+            assertThat(actualBranches).doesNotContain("feature-1")
+        }
+
+    @Test
+    fun `the file should contain the original content after the commit was deleted`() =
+        testDriver(tmpDir) {
+            createLocalRepository()
+
+            repeat(2) {
+                File("$client1WorkDir/newFile.txt").writeText("Commit number $it")
+                client1Api.add("newFile.txt")
+                client1Api.commit("commit number $it")
+            }
+
+            val commandResult = client1Api.deleteLastCommit()
+            assertThat(commandResult.isOk()).isTrue()
+
+            val actual = File("$client1WorkDir/newFile.txt").readText()
+            assertThat(actual).isEqualTo("Commit number 0")
+        }
+
+    @Test
+    fun `the file should be deleted when the commit is reverted where the file was added`() =
+        testDriver(tmpDir) {
+            createLocalRepository()
+
+            val createdFile = File("$client1WorkDir/newFile.txt").apply { writeText("Hello World") }
+            client1Api.add("newFile.txt").assertIsOk()
+            client1Api.commit("Add new file").assertIsOk()
+
+            client1Api.deleteLastCommit().assertIsOk()
+
+            assertThat(createdFile).doesNotExist()
+        }
+
+    @Test
+    fun `should be able to set a remote branch and push a git commit that is remote available`() =
+        testDriver(tmpDir) {
+            serverApi.initBareRepository()
+
+            createLocalRepository()
+            client1Api.addRemoteAndSetUpstream("origin", serverWorkDir.absolutePath, "main")
+            client1Api.push().assertIsOk()
+
+            // Check can the branch be checked out
+            client2Api.clone(serverWorkDir.absolutePath, ".", "main").assertIsOk()
+            assertThat(client2WorkDir.resolve("build.gradle.kts")).exists()
+        }
+
+    @Test
+    fun `git push should fail if another client has already pushed a commit`() =
+        testDriver(tmpDir) {
+            createValidRepositoryWithRemote()
+
+            // Push from another client
+            cloneForClient2()
+            client2WorkDir.resolve("other.txt").apply { writeText("Hello World") }
+            client2Api.add("other.txt").assertIsOk()
+            client2Api.commit("Second commit").assertIsOk()
+            client2Api.push().assertIsOk()
+
+            // Original client should fail
+            client1WorkDir.resolve("something.txt").apply { writeText("Hello World") }
+            client1Api.add("something.txt").assertIsOk()
+            client1Api.commit("Third commit").assertIsOk()
+            val actual = client1Api.push()
+
+            assertThat(actual.isLeft()).isTrue()
+        }
+
+    @Test
+    fun `should be able to rebase the commits if another client has already pushed changes`() =
+        testDriver(tmpDir) { TODO("Not yet implemented") }
+
+    @Test
+    fun `should be able to create and list tags`() =
+        testDriver(tmpDir) {
+            createLocalRepository()
+
+            client1Api.tag("v1.0", "Initial release").assertIsOk()
+            client1Api.tag("v1.1", "Second release").assertIsOk()
+
+            val actual = client1Api.listTags().assertIsOk()
+
+            assertThat(actual).containsExactly("v1.0", "v1.1")
+        }
+
+    @Test
+    fun `should be able to delete a tag`() =
+        testDriver(tmpDir) {
+            createLocalRepository()
+
+            client1Api.tag("v1.0", "Initial release").assertIsOk()
+            client1Api.tag("v1.1", "Second release").assertIsOk()
+
+            client1Api.deleteLocalTag("v1.0").assertIsOk()
+
+            val actual = client1Api.listTags().assertIsOk()
+            assertThat(actual).containsExactly("v1.1")
+        }
 }
