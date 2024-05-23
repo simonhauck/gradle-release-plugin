adb6d3281a3631b238d95a4cf42b8860bd5fe88f
Simon Hauck <simon.hauck@bruker.com>
2024-05-23 21:45:02 +0200

    #20 simplify example

--- example/build.gradle.kts
+++ example/build.gradle.kts
@@ -1,4 +1,3 @@
-import io.github.simonhauck.release.tasks.BaseReleaseTask
 import io.github.simonhauck.release.version.api.Version
 
 plugins {
@@ -14,14 +13,9 @@ val versionFile = layout.projectDirectory.file("version.properties")
 // You can set the version from the version.properties file. You can utilize this helper method
 version = Version.fromPropertiesFile(versionFile.asFile)
 
-tasks.withType<BaseReleaseTask> {
-    // This is just here, because it interferes with the release task of this parent project
-    val include = project.path.startsWith(":example")
-    onlyIf { include }
-}
-
 // This is the configuration object
 release {
-    // Here you can specify all the configuration options
     disablePush = true
+    // Here you can specify all the configuration options
+    // ...
 }
