d79cf844b7b00b593f6689957aeecd261e78815c
Simon Hauck <simon.hauck@bruker.com>
2024-05-23 19:46:25 +0200

    #20 replace kotlinLogger with gradle logger

--- release-plugin/src/main/kotlin/io/github/simonhauck/release/tasks/CalculateReleaseVersionTask.kt
+++ release-plugin/src/main/kotlin/io/github/simonhauck/release/tasks/CalculateReleaseVersionTask.kt
@@ -1,12 +1,12 @@
 package io.github.simonhauck.release.tasks
 
-import io.github.oshai.kotlinlogging.KotlinLogging
 import io.github.simonhauck.release.version.api.ReleaseVersions
 import io.github.simonhauck.release.version.api.Version
 import io.github.simonhauck.release.version.api.VersionHolderApi
 import io.github.simonhauck.release.version.api.VersionIncrementStrategyParserApi
 import java.io.File
 import org.gradle.api.file.RegularFileProperty
+import org.gradle.api.logging.Logging
 import org.gradle.api.provider.MapProperty
 import org.gradle.api.provider.Property
 import org.gradle.api.tasks.Input
@@ -14,9 +14,9 @@ import org.gradle.api.tasks.InputFile
 import org.gradle.api.tasks.OutputFile
 import org.gradle.api.tasks.TaskAction
 
-private val log = KotlinLogging.logger {}
-
 abstract class CalculateReleaseVersionTask : BaseReleaseTask() {
+
+    private val log = Logging.getLogger(this::class.java)
 
     init {
         description = "Calculate the release version and the next development version"
@@ -35,7 +35,7 @@ abstract class CalculateReleaseVersionTask : BaseReleaseTask() {
             versionHolder.loadVersionFromFileOrThrow(versionPropertyFile.get().asFile)
         val releaseVersions = getReleaseVersions(currentVersion)
 
-        log.info { "Determined versions $releaseVersions" }
+        log.lifecycle("Determined versions $releaseVersions")
 
         versionHolder.saveVersions(releaseVersions)
     }
--- release-plugin/src/main/kotlin/io/github/simonhauck/release/version/internal/VersionIncrementStrategyParser.kt
+++ release-plugin/src/main/kotlin/io/github/simonhauck/release/version/internal/VersionIncrementStrategyParser.kt
@@ -1,14 +1,13 @@
 package io.github.simonhauck.release.version.internal
 
-import io.github.oshai.kotlinlogging.KotlinLogging
 import io.github.simonhauck.release.version.api.ReleaseVersions
 import io.github.simonhauck.release.version.api.Version
 import io.github.simonhauck.release.version.api.VersionIncrementStrategyParserApi
 import org.gradle.api.GradleException
-
-private val log = KotlinLogging.logger {}
+import org.gradle.api.logging.Logging
 
 internal class VersionIncrementStrategyParser : VersionIncrementStrategyParserApi {
+    private val log = Logging.getLogger(this::class.java)
 
     private val parsers =
         listOf(
@@ -23,13 +22,15 @@ internal class VersionIncrementStrategyParser : VersionIncrementStrategyParserAp
         val parsedVersion = parsers.firstNotNullOfOrNull { it.tryParse(currentVersion, parameters) }
 
         if (parsedVersion == null) {
-            log.error { "No version increment strategy found" }
+            log.error("No version increment strategy found")
             parsers.forEach {
-                log.error { "Available strategy: ${it.strategyName}" }
-                it.requiredPropertyDescription.forEach { log.error { "\t $it" } }
+                log.error("Available strategy: ${it.strategyName}")
+                it.requiredPropertyDescription.forEach { log.error("\t $it") }
             }
 
-            throw GradleException("No valid version increment strategy found.")
+            throw GradleException(
+                "No valid version increment strategy found. Check the log or the documentation for the available parameters"
+            )
         }
 
         return parsedVersion
