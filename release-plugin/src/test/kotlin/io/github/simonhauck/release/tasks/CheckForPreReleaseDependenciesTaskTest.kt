fa247136e9ebb3591140a00ad37fc59b92b84f7e
SimonHauck <simonhauck@gmx.de>
2024-08-23 21:32:28 +0000

    #69 implement missing tests for CheckForPreReleaseDependenciesTask

--- release-plugin/src/test/kotlin/io/github/simonhauck/release/tasks/CheckForPreReleaseDependenciesTaskTest.kt
+++ release-plugin/src/test/kotlin/io/github/simonhauck/release/tasks/CheckForPreReleaseDependenciesTaskTest.kt
@@ -43,7 +43,8 @@ class CheckForPreReleaseDependenciesTaskTest {
                     "> Found 2 dependencies with a pre-release version that are not allowed",
                     "   - other.xy:lib2:1.0.0-SNAPSHOT",
                     "   - some.group:lib:1.0.0-alpha",
-                    "  Change the versions or add them to the ignore list.")
+                    "  Change the versions or add them to the ignore list.",
+                )
         }
 
     @Test
@@ -66,22 +67,93 @@ class CheckForPreReleaseDependenciesTaskTest {
         }
 
     @Test
-    fun `should not list snapshot dependencies that are specified in the ignore list`() {
-        TODO("Not yet implemented")
-    }
+    fun `should not list snapshot dependencies that are specified in the ignore list`() =
+        testDriver(tmpDir) {
+            appendContentToBuildGradle(
+                """
+                |tasks.register<CheckForPreReleaseDependenciesTask>("testCheckForPreReleaseDependencies") {
+                |    usedDependencies = setOf(
+                |       $snapshotVersionDependency,
+                |    )
+                |    ignorePreReleaseDependencies = listOf($snapshotVersionDependency)
+                |}
+                """
+                    .trimMargin())
+
+            val runner = testKitRunner().withArguments("testCheckForPreReleaseDependencies").build()
+            val actual = runner.task(":testCheckForPreReleaseDependencies")
+
+            assertThat(actual?.outcome).isEqualTo(TaskOutcome.SUCCESS)
+        }
 
     @Test
-    fun `should not list snapshot dependencies that are specified in the ignore file`() {
-        TODO("Not yet implemented")
-    }
+    fun `should not list snapshot dependencies that are specified in the ignore file`() =
+        testDriver(tmpDir) {
+            appendContentToBuildGradle(
+                """
+            |tasks.register<CheckForPreReleaseDependenciesTask>("testCheckForPreReleaseDependencies") {
+            |    usedDependencies = setOf(
+            |       $snapshotVersionDependency,
+            |    )
+            |    ignorePreReleaseDependenciesFile = file("ignore.txt")
+            |}
+            """
+                    .trimMargin())
+            client1WorkDir
+                .resolve("ignore.txt")
+                .writeText(snapshotVersionDependency.replace("\"", ""))
+
+            val runner = testKitRunner().withArguments("testCheckForPreReleaseDependencies").build()
+            val actual = runner.task(":testCheckForPreReleaseDependencies")
+
+            assertThat(actual?.outcome).isEqualTo(TaskOutcome.SUCCESS)
+        }
 
     @Test
     fun `should not fail if a version has an unexpected format`() {
-        TODO("Not yet implemented")
+        testDriver(tmpDir) {
+            appendContentToBuildGradle(
+                """
+                |tasks.register<CheckForPreReleaseDependenciesTask>("testCheckForPreReleaseDependencies") {
+                |    usedDependencies = setOf(
+                |       "\"some.group:lib:1.0.0.a1\"",
+                |       "\"other.xy:lib2:1.0\"",
+                |       "\"released.dep:great:1.0.0-1.2.6\"",
+                |    )
+                |}
+                """
+                    .trimMargin())
+
+            val runner = testKitRunner().withArguments("testCheckForPreReleaseDependencies").build()
+            val actual = runner.task(":testCheckForPreReleaseDependencies")
+
+            assertThat(actual?.outcome).isEqualTo(TaskOutcome.SUCCESS)
+        }
     }
 
     @Test
     fun `task should be up to date when invoked twice`() {
-        TODO("Not yet implemented")
+        testDriver(tmpDir) {
+            appendContentToBuildGradle(
+                """
+                |tasks.register<CheckForPreReleaseDependenciesTask>("testCheckForPreReleaseDependencies") {
+                |    usedDependencies = setOf(
+                |       $releaseVersionDependency,
+                |    )
+                |}
+                """
+                    .trimMargin())
+
+            val runner = testKitRunner().withArguments("testCheckForPreReleaseDependencies").build()
+            val actual = runner.task(":testCheckForPreReleaseDependencies")
+
+            assertThat(actual?.outcome).isEqualTo(TaskOutcome.SUCCESS)
+
+            val secondRunner =
+                testKitRunner().withArguments("testCheckForPreReleaseDependencies").build()
+            val secondActual = secondRunner.task(":testCheckForPreReleaseDependencies")
+
+            assertThat(secondActual?.outcome).isEqualTo(TaskOutcome.UP_TO_DATE)
+        }
     }
 }
