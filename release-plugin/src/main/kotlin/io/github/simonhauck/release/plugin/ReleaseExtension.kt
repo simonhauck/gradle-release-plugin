c1de7736578da196c3ea505516605b3dc584d578
simonhauck <simonhauck@gmx.de>
2024-08-28 21:22:45 +0000

    #69 reorder extension & make them more compact for better readability and structure

--- release-plugin/src/main/kotlin/io/github/simonhauck/release/plugin/ReleaseExtension.kt
+++ release-plugin/src/main/kotlin/io/github/simonhauck/release/plugin/ReleaseExtension.kt
@@ -1,54 +1,60 @@
 package io.github.simonhauck.release.plugin
 
-import java.io.File
-import java.time.Duration
 import org.gradle.api.file.ProjectLayout
 import org.gradle.api.file.RegularFileProperty
 import org.gradle.api.model.ObjectFactory
 import org.gradle.api.provider.ListProperty
 import org.gradle.api.provider.Property
+import org.gradle.api.provider.Provider
+import java.io.File
+import java.time.Duration
 
 abstract class ReleaseExtension(
-    objects: ObjectFactory,
-    layout: ProjectLayout,
+    private val objects: ObjectFactory,
+    private val layout: ProjectLayout,
 ) {
-    val rootGitDirectory: RegularFileProperty =
-        objects.fileProperty().convention(layout.projectDirectory.file("./"))
+    // Project properties
+    val rootGitDirectory: RegularFileProperty = projectFileProperty("./")
+    val versionPropertyFile: RegularFileProperty = projectFileProperty("version.properties")
 
-    val versionPropertyFile: RegularFileProperty =
-        objects.fileProperty().convention(layout.projectDirectory.file("version.properties"))
+    // Checks
+    val checkForUncommittedFiles: Property<Boolean> = booleanProperty(true)
 
+    // Git config
+    val gitName: Property<String> = stringProperty()
+    val gitEmail: Property<String> = stringProperty()
+    val sshKeyFile: RegularFileProperty = fileProperty()
+    val commitMessagePrefix: Property<String> = stringProperty("")
+    val disablePush: Property<Boolean> = booleanProperty(false)
+
+    // Release commit
     val releaseCommitAddFiles: ListProperty<File> =
-        objects
-            .listProperty(File::class.java)
-            .convention(versionPropertyFile.map { listOf(it.asFile) })
+        fileListProperty(versionPropertyFile.map { listOf(it.asFile) })
+    val tagName: Property<String> = stringProperty("v{version}")
+    val releaseCommitMessage: Property<String> = stringProperty("Release commit: v{version}")
 
-    val releaseCommitMessage: Property<String> =
-        objects.property(String::class.java).convention("Release commit: v{version}")
-
+    // Post release commit
     val postReleaseCommitAddFiles: ListProperty<File> =
-        objects
-            .listProperty(File::class.java)
-            .convention(versionPropertyFile.map { listOf(it.asFile) })
+        fileListProperty(versionPropertyFile.map { listOf(it.asFile) })
+    val postReleaseCommitMessage = stringProperty("Post release commit: v{version}")
+    val delayBeforePush: Property<Duration> = durationProperty(Duration.ZERO)
 
-    val postReleaseCommitMessage: Property<String> =
-        objects.property(String::class.java).convention("Post release commit: v{version}")
+    // Utility methods
 
-    val commitMessagePrefix: Property<String> = objects.property(String::class.java).convention("")
+    private fun stringProperty(default: String? = null): Property<String> =
+        objects.property(String::class.java).convention(default)
 
-    val tagName: Property<String> = objects.property(String::class.java).convention("v{version}")
+    private fun projectFileProperty(default: String?): RegularFileProperty =
+        objects.fileProperty().convention(default?.let { layout.projectDirectory.file(it) })
 
-    val gitName: Property<String> = objects.property(String::class.java)
+    private fun fileProperty(): RegularFileProperty = objects.fileProperty()
 
-    val gitEmail: Property<String> = objects.property(String::class.java)
+    private fun booleanProperty(default: Boolean? = null): Property<Boolean> =
+        objects.property(Boolean::class.java).convention(default)
 
-    val sshKeyFile: RegularFileProperty = objects.fileProperty()
+    private fun fileListProperty(default: Provider<List<File>>): ListProperty<File> =
+        objects.listProperty(File::class.java).convention(default)
 
-    val disablePush: Property<Boolean> = objects.property(Boolean::class.java).convention(false)
-
-    val delayBeforePush: Property<Duration> =
-        objects.property(Duration::class.java).convention(Duration.ZERO)
-
-    val checkForUncommittedFiles: Property<Boolean> =
-        objects.property(Boolean::class.java).convention(true)
+    private fun durationProperty(default: Duration? = null): Property<Duration> =
+        objects.property(Duration::class.java).convention(default)
 }
